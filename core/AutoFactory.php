5ff2c4d62c1cd00a368c9adcc88a2debce259f79
stefan-spitse <97093546@st.deltion.nl>
2023-06-09 09:25:37 +0200

    Fixed photos not showing first time on webpage.

--- core/AutoFactory.php
+++ core/AutoFactory.php
@@ -2,25 +2,25 @@
 
 function AutoFactory($autoConstructor)
 {
-    if (isset($_GET['submit'])) {
-        $autos = $autoConstructor->getAuto($_GET['merk']);
 
-        $min = isset($_GET['min']) ? $_GET['min'] : null;
-        $max = isset($_GET['max']) ? $_GET['max'] : null;
-        $autos = $autoConstructor->MaxPrice($autos, $max);
-        $autos = $autoConstructor->MinPrice($autos, $min);
-        $out = "<table>";
-        foreach ($autos as $auto) {
-            $out .= "<tr><td>";
-            $out .= '<img class="auto img-thumbnail" src="' . $auto->photo . '"></td>';
-            $out .= '<td><p>' . $auto->merk . " " . $auto->type . " " . ' prijs:€ ' . $auto->prijs . ' </p></td></tr>';
+    $autos = $autoConstructor->getAuto($_GET['merk']);
+
+    $min = isset($_GET['min']) ? $_GET['min'] : null;
+    $max = isset($_GET['max']) ? $_GET['max'] : null;
+    $autos = $autoConstructor->MaxPrice($autos, $max);
+    $autos = $autoConstructor->MinPrice($autos, $min);
+    $out = "<table>";
+    foreach ($autos as $auto) {
+        $out .= "<tr><td>";
+        $out .= '<img class="auto img-thumbnail" src="' . $auto->photo . '"></td>';
+        $out .= '<td><p>' . $auto->merk . " " . $auto->type . " " . ' prijs:€ ' . $auto->prijs . ' </p></td></tr>';
 
 
-        }
-        $out .= "</table>";
-
-        return $out;
     }
+    $out .= "</table>";
+
+    return $out;
+
 
 }
 
